07a64d827a1152905044b918cf0202d8ce66b6b7
gitbreno <breno.gusta@gmail.com>
2024-06-06 03:34:40 -0300

    ler mais e ler menos

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const searchButton = document.getElementById('search-button');
-    const searchInput = document.getElementById('search-input');
+document.addEventListener("DOMContentLoaded", () => {
+    const readMoreBtn = document.querySelector(".read-more-btn");
+    const readLessBtn = document.querySelector(".read-less-btn");
+    const moreText = document.querySelector(".more-text");
 
-    searchButton.addEventListener('click', function () {
-        const searchText = searchInput.value.toLowerCase();
-        const menuItems = document.querySelectorAll('.menu li a');
-
-        menuItems.forEach(item => {
-            const itemText = item.textContent.toLowerCase();
-            const sectionId = item.getAttribute('href').substring(1);
-            const section = document.getElementById(sectionId);
-
-            if (itemText.includes(searchText)) {
-                section.scrollIntoView({ behavior: 'smooth' });
-                section.style.transition = 'background-color 1s ease';
-                section.style.backgroundColor = 'blue'; // Highlight the section
-                
-                setTimeout(() => {
-                    section.style.backgroundColor = ''; // Remove the highlight after some time
-                }, 3000);
-            }
-        });
+    readMoreBtn.addEventListener("click", () => {
+        moreText.style.display = "block";
+        readMoreBtn.style.display = "none";
+        readLessBtn.style.display = "inline-block";
     });
 
-    const menuLinks = document.querySelectorAll('.menu li a');
-    menuLinks.forEach(link => {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href').substring(1);
-            const targetSection = document.getElementById(targetId);
-            targetSection.scrollIntoView({ behavior: 'smooth' });
-        });
+    readLessBtn.addEventListener("click", () => {
+        moreText.style.display = "none";
+        readMoreBtn.style.display = "inline-block";
+        readLessBtn.style.display = "none";
     });
 });
